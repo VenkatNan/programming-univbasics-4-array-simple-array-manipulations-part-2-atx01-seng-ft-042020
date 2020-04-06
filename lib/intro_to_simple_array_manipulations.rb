660a73593e5d944c9c5d9168a57329a8ba84b1da
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-06 16:45:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,11 +2,11 @@ def using_concat (array1,array2)
   
 end
 
-def using_concat (array1,array2)
+def using_insert (array1,array2)
   
 end
 
-def using_concat (array1,array2)
+def using_uniq (array)
   
 end
 
